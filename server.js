c4189a1ec38d05d2e541adaece141115e2623714
unknown <kentmbox42@yahoo.com>
2018-03-10 12:13:16 -0600

    changed file directory structure

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ require('./routing/apiRoutes.js')(app);
 var friends = require("./data/friends.js");
 app.use(express.static("./public"));
 
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 //app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(bodyParser.json());
 
 
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+    console.log("App listening on PORT " + port);
 });
